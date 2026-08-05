20fd7ebbcd4766b60c234deecd43f3889757452b
Sutta <msutta0@gmail.com>
2026-08-06 05:47:39 +0700

    Deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/portofolio-v3-fresh/',
+  base: '/portofolio-v3/',
   plugins: [react(), 
            tailwindcss()],
 });
