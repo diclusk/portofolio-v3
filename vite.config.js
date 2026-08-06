7177200a2065b0db82f10c45d9c9e2dccf305e5b
Sutta <msutta0@gmail.com>
2026-08-06 07:24:04 +0700

    published

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-<<<<<<< HEAD
   base: '/portofolio-v3/',
-=======
-  base: './',
->>>>>>> ae14e8d4b0748ca05deb60912d946369d2fd64b2
   plugins: [react(), 
            tailwindcss()],
 });
